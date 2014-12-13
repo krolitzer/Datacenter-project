72372640e26805019b0f4f865b9eea81137cea53
Austin Longo <alongo117@gmail.com>
2014-12-13 15:19:38 -0700

    Attempt 3 at ssh key; Removed old users.pp

--- users_austin.pp
+++ users_austin.pp
@@ -31,8 +31,8 @@ file { "/home/austin/.ssh":
 
 ssh_authorized_key { "austin":
         ensure => present,
-        name => "alongo117@gmail.com",
-        user => "austin",
+        name => "austin",
+        user => "alongo117@gmail.com",
         type => 'ssh-dss',
         key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC1f8pzhKMcgDJk83G2KFc8hH7mt68w8aGA8VVMQ8QXPAS18sxYT2LU0xHTwK0ceDn7JXMltPU9xKDw0i3xJenbZiSDjW/F0Piohadf6dsIb1rWp/tQbR/mWFoJllgNBgMuxqVxc2alQblzu14LksRsbNl0PSzD6iV2RDiactzbnBEUE6j2vCkas0r9XybCcKrdPKysnjmYbik/p86JX50Swn0/A/+Qipt9UxAdPhuipyjNvlpPLBYaAt6oLz9xMPqiuRLbRujAZry+3OaVxyNIE4cD94Nn/d0CyvdziCU0yKOoG2N4/ssIiak4i+3VQrtpSVdD5MZEhuODqwKxCqV5',
 }
