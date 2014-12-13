e156973678ae5e792d845fe2d90f0d9cc81d237e
Austin Longo <alongo117@gmail.com>
2014-12-13 13:29:05 -0700

    austin's puppet user file

--- users_austin.pp
+++ users_austin.pp
@@ -1,20 +1,20 @@
-user { "chris":
+user { "austin":
         uid => "1117",
         ensure => present,
-        gid => "chris",
+        gid => "austin",
         membership => minimum,
         shell => "/bin/bash",
-        home => "/home/chris",
+        home => "/home/austin",
 }
 
-group { "chris":
+group { "austin":
         gid => 1117,
 }
 
-ssh_authorized_key { "chris":
+ssh_authorized_key { "austin":
         ensure => present,
-        name => "chris",
-        user => "chris",
+        name => "austin",
+        user => "austin",
         type => 'ssh-dss',
         key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC1f8pzhKMcgDJk83G2KFc8hH7mt68w8aGA8VVMQ8QXPAS18sxYT2LU0xHTwK0ceDn7JXMltPU9xKDw0i3xJenbZiSDjW/F0Piohadf6dsIb1rWp/tQbR/mWFoJllgNBgMuxqVxc2alQblzu14LksRsbNl0PSzD6iV2RDiactzbnBEUE6j2vCkas0r9XybCcKrdPKysnjmYbik/p86JX50Swn0/A/+Qipt9UxAdPhuipyjNvlpPLBYaAt6oLz9xMPqiuRLbRujAZry+3OaVxyNIE4cD94Nn/d0CyvdziCU0yKOoG2N4/ssIiak4i+3VQrtpSVdD5MZEhuODqwKxCqV5',
 }
